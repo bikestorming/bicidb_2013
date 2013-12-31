b5280d891fb1bec407a96eefb2213d4dba0750f2
Pablo Terradillos <tehsis@gmail.com>
2013-12-31 12:22:21 -0300

    Added username support

--- controllers/missions/bikechecks/index.js
+++ controllers/missions/bikechecks/index.js
@@ -9,6 +9,7 @@ exports.save = function(req, res) {
   var image_path =  image_name + ".jpg";
   var image = req.body.image.url.replace(/^data:image\/jpeg;base64,/,"");
   var thumbnail_path = image_name + '-thumbnail' + ".jpg"; 
+
   fs.writeFile("./images/" + image_path,new Buffer(image, "base64"), function(err) {
       var new_bikecheck = new bikecheck({
         location: req.body.location,
@@ -16,21 +17,22 @@ exports.save = function(req, res) {
         image: {
           original: '/images/' + image_path,
           thumbnail: '/images/' + thumbnail_path
-        }
+        },
+        user: req.body.user
       }); 
+
      easyimage.resize({
        src: "./images/" + image_path,
        dst: "./images/" + thumbnail_path,
        width: 100,
        height: 100
-     }, function(image) {
-       console.log(image);
+     }, function() {
      });
+
      new_bikecheck.save(function() {
-      console.log('se salvo!');
+      console.log('New Bikecheck');
     });
   });
-       
    
   res.send(200);
 };
